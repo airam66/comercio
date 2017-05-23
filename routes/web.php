00b6f560a4854f13b70cac1253e1721cf22ca271
Yanina242 <yaninadaianamartinez@gmail.com>
2017-05-22 22:50:20 -0300

    Agregar nueva marca

--- routes/web.php
+++ routes/web.php
@@ -32,8 +32,7 @@ Route::resource('cotillon','MainPagineController');
 Route::group(['prefix'=>'admin'], function(){
 
   Route::resource('categories','CategoriesController');
-  Route::resource('events','EventController');
-  
+  Route::resource('brands','BrandController');  
   Route::resource('products','ProductsController');
 
   Route::resource('lines','LinesController');
