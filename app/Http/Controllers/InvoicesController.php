b07b745c611d653072a83aad32c1c2d2f0d36c96
airam66 <fairam66@gmail.com>
2017-07-03 19:43:04 -0300

    Update InvoicesController.php

--- app/Http/Controllers/InvoicesController.php
+++ app/Http/Controllers/InvoicesController.php
@@ -164,7 +164,7 @@ public function searchDate(Request $request){
       $invoices=Invoice::SearchInvoice($request->fecha1,$request->fecha2)->get();
        if ($invoices) {
         foreach ($invoices as $key => $invoice) {
-          $dir="{{route('invoice.desable',".$invoice->id.")}}";
+         
                 if ($invoice->status!='inactivo'){
                   $output .='<tr role="row" class="odd">';
                 }
@@ -209,7 +209,7 @@ public function searchDate(Request $request){
         $invoice= Invoice::find($request->id);
         $invoice->status='inactivo';
         $invoice->save();
-        ajax();
+    
         return redirect()->route('admin.invoices.index');
     }
 
@@ -236,129 +236,6 @@ public function searchDate(Request $request){
     
       return view ('admin.invoices.show')->with('invoice',$invoice)
                                           ->with('detalles',$detalles);
-    }
-
-
-    public function print(Request $request){
-      
-
-      if($request->ajax()){
-
-      $primera="";
-      $segunda="";
-
-      $invoice= Invoice::find($request->id);
-      $detalles= DB::table('invoices_products as d')
-      ->join('products as p','d.product_id','=','p.id')
-      ->select('p.id','p.name','p.description','d.amount','d.subTotal')
-      ->where('d.invoice_id','=',$request->id)->get();
-      
-      $primera='<div class="wrapper">
-      <!-- Main content -->
-      <section class="invoice">
-        <!-- title row -->
-        <div class="row">
-          <div class="col-xs-12">
-            <h2 class="page-header">
-              <i class="fa fa-globe"></i> Cotillon CreaTú
-              <small class="pull-right">Fecha:'.$invoice->created_at.'</small>
-            </h2>
-          </div><!-- /.col -->
-        </div>
-        <!-- info row -->
-        <div class="row invoice-info">
-          <div class="col-sm-6 invoice-col">
-            DE
-            <address>
-              <strong>Cotillon creaTu</strong><br>
-              Direccion:Roque Saenz Peña Nro 14 bis 2 <br>
-              B° San Martin,Rosario de Lerma, Salta<br>
-              Telefono: (387)59662005 - (387) 5910201<br>
-              Email:creatucotillon@gmail.com
-            </address>
-          </div><!-- /.col -->
-          <div class="col-sm-6 invoice-col">
-            A
-            <address>
-              <strong>John Doe</strong><br>
-              795 Folsom Ave, Suite 600<br>
-              San Francisco, CA 94107<br>
-              Phone: (555) 539-1037<br>
-              Email: john.doe@example.com
-            </address>
-          </div><!-- /.col -->
-          
-        </div><!-- /.row -->
-
-        <!-- Table row -->
-        <br>
-        <div class="row">
-          <div class="col-xs-12 table-responsive">
-            <table class="table table-striped">
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Producto</th>
-                  <th>Descripcion</th>
-                  <th>Cantidad </th>
-                  <th>Subtotal</th>
-                </tr>
-              </thead>
-              <br>
-              <tbody>';
-              foreach ($detalles as $key => $detalle) {
-                 $segunda='<tr>
-                          <td>'.$detalle->id.'</td>
-                          <td>'.$detalle->name.'</td>
-                          <td>'.$detalle->description.'</td>
-                          <td>'.$detalle->amount.'</td>
-                          <td>'.$detalle->subTotal.'</td>
-                        </tr>';
-                }
-              $primera=$primera.$segunda.'</tbody>
-            </table>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-
-        <div class="row">
-          <!-- accepted payments column -->
-          <div class="col-xs-6">
-            <p class="lead">Payment Methods:</p>
-            <img src="../../dist/img/credit/visa.png" alt="Visa">
-            <img src="../../dist/img/credit/mastercard.png" alt="Mastercard">
-            <img src="../../dist/img/credit/american-express.png" alt="American Express">
-            <img src="../../dist/img/credit/paypal2.png" alt="Paypal">
-            <p class="text-muted well well-sm no-shadow" style="margin-top: 10px;">
-              Comprbante no valido como factura. Contillón Creatu.
-            </p>
-          </div><!-- /.col -->
-          <div class="col-xs-6">
-            <p class="lead">Amount Due 2/22/2014</p>
-            <div class="table-responsive">
-              <table class="table">
-                <tr>
-                  <th style="width:50%">Subtotal:</th>
-                  <td>'.$invoice->total.'</td>
-                </tr>
-                <tr>
-                  <th>Descuento</th>
-                  <td>'.$invoice->total.'%</td>
-                </tr>
-                <tr>
-                  <th>Total:</th>
-                  <td>'.$invoice->total.'</td>
-                </tr>
-              </table>
-            </div>
-          </div><!-- /.col -->
-        </div><!-- /.row -->      </section><!-- /.content -->
-    </div><!-- ./wrapper -->
-
-    ';
-              }
-
-    return Response($primera);
-
     }
 
 
