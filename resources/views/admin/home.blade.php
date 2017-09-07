59f84f081e15420a62785757d7f00942652a68ef
lamasg92 <gabrielcja3@gmail.com>
2017-09-06 22:48:17 -0300

    Rep Art mas vendidos - cambios de shopping card

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -12,27 +12,28 @@
           <h1> <b>Bienvenido al panel de administración</b></h1>
        
           <!-- /.box-body -->
-        </div>
-      <div class="box box-info">
         <div class="row text-center">
-        <h3> <b>Accesos rapidos</b></h3>
+        <h3> <b>    </b></h3>
         </div>
+        
         <div class="row text-center">
-          <div class="col-md-6 text-center">
+          <div class="col-md-7 text-center">
             <a href="{{route('products.index')}}" id="ProductIndex"><button><img src="{{ asset('images/main/list.png')}}" width="150" height="150"><br> Lista de productos</button></a>
           </div>
-          <div class="col-md-5 text-center">
+          <div class="col-md-1 text-center">
             <a href="{{route('invoices.create')}}" id="sales"><button><img src="{{ asset('images/main/sale.png')}}" width="150" height="150"><br> Nueva venta</button></a>
           </div>
         </div>
+        <br>
         <div class="row text-center">
-          <div class="col-md-6 text-center">
+          <div class="col-md-7 text-center">
             <a href="{{route('orders.create')}}" id="orders"><button><img src="{{ asset('images/main/truck.png')}}" width="150" height="150"><br> Nuevo pedido</button></a>
           </div>
-          <div class="col-md-5 text-center">
+          <div class="col-md-1 text-center">
             <a href="{{route('calendar')}}" target="_blank" id="calendar"><button><img src="{{ asset('images/main/calendar.png')}} " width="150" height="150"><br> Calendario</button></a>
           </div>
         </div>
+        <br>
       </div>
            
         <!-- /.box -->
